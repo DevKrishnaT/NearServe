b128a606fd1071505e819648d7cff5f0275a84b8
DevKrishnaT <krishnasehai1@gmail.com>
2026-04-22 21:10:00 +0530

    actual change

--- backend/server/main.js
+++ backend/server/main.js
@@ -46,10 +46,10 @@ app.listen(4000, async () => {
   try {
     await pool.query("SELECT 1");
 
-    console.log("✅ DB connected s");
-    console.log("🚀 Server running on http://localhost:4000");
+    console.log(" DB connected s");
+    console.log(" Server running on http://localhost:4000");
   } catch (err) {
-    console.error("❌ DB connection failed:", err.message);
+    console.error(" DB connection failed:", err.message);
     process.exit(1);
   }
 });
